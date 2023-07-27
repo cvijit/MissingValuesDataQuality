b910dcb9d32b154c6477c42bbc82635e50e756e4
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2023-07-27 09:37:32 +0100

    Update DealingMissingValues.py

--- DealingMissingValues.py
+++ DealingMissingValues.py
@@ -1,59 +1,51 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import dtale
+import klib
 
-def handle_missing_values(df, consider_zero_as_null=False):
-    # Remove rows with missing values based on user's preference
-    if consider_zero_as_null:
-        df_cleaned = df.replace({0: np.nan}).dropna()
-    else:
-        df_cleaned = df.dropna()
+def handle_missing_values_with_klib(df):
+    # Handle missing values using klib
+    df_cleaned = klib.data_cleaning(df)
     return df_cleaned
 
-def main():
-    st.title("Data Quality Tool")
-    st.write("Upload your dataset to handle missing values!")
+def handle_missing_values_with_dtale(df):
+    # Handle missing values using dtale
+    dtale_app = dtale.show(df)
+    dtale_app.open_browser()
+    return df
 
+def main():
+    st.title("Data Analysis and Manipulation App")
+
+    # Upload file
     uploaded_file = st.file_uploader("Upload CSV or Excel file", type=["csv", "xlsx"])
 
     if uploaded_file is not None:
-        try:
-            df = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('.csv') else pd.read_excel(uploaded_file)
-            st.write("Original Dataset:")
-            st.write(df)
+        df = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('.csv') else pd.read_excel(uploaded_file)
 
-            # Option to choose between blank values and 0 as null values
-            consider_zero_as_null = st.checkbox("Consider 0 as Null Values")
+        st.subheader("Preview of the Dataset:")
+        st.dataframe(df)
 
-            # Show button to view dataset with highlighted missing values
-            if st.button("View Dataset with Missing Values"):
-                st.write("Dataset with Missing Values:")
-                df_with_missing_values = df.copy()
-                missing_values_mask = df_with_missing_values.isnull()
-                st.dataframe(df_with_missing_values.style.highlight_null(null_color='red'))
+        if st.button("Use klib for Missing Data Handling"):
+            df_cleaned_klib = handle_missing_values_with_klib(df)
+            st.subheader("Dataset after handling missing values using klib:")
+            st.dataframe(df_cleaned_klib)
 
-            # Handle missing values based on user's preference
-            df_cleaned = handle_missing_values(df, consider_zero_as_null)
+        if st.button("Use dtale for Missing Data Handling"):
+            df_cleaned_dtale = handle_missing_values_with_dtale(df)
+            st.subheader("Dataset after handling missing values using dtale:")
+            st.dataframe(df_cleaned_dtale)
 
-            # Show button to delete rows with missing values
-            if st.button("Delete Rows with Missing Values"):
-                st.write("Dataset after handling missing values:")
-                st.write(df_cleaned)
+        # Export the cleaned dataset
+        export_format = st.selectbox("Export Format:", ["CSV", "Excel"])
 
-            # Allow user to export the cleaned dataset
-            export_format = st.selectbox("Export Format:", ["CSV", "Excel"])
-            if st.button("Export"):
-                if export_format == "CSV":
-                    st.write("Exporting as CSV...")
-                    df_cleaned.to_csv("cleaned_data.csv", index=False)
-                    st.success("Data exported to cleaned_data.csv")
-                elif export_format == "Excel":
-                    st.write("Exporting as Excel...")
-                    df_cleaned.to_excel("cleaned_data.xlsx", index=False)
-                    st.success("Data exported to cleaned_data.xlsx")
-
-        except Exception as e:
-            st.error(f"Error occurred: {e}")
+        if st.button("Export Cleaned Data"):
+            if export_format == "CSV":
+                df.to_csv("cleaned_data.csv", index=False)
+                st.success("Data exported to cleaned_data.csv")
+            elif export_format == "Excel":
+                df.to_excel("cleaned_data.xlsx", index=False)
+                st.success("Data exported to cleaned_data.xlsx")
 
 if __name__ == "__main__":
     main()
